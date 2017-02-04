50f13fd193efbe5fcc951878e63dddea463efd51
rei.shibano <shibano@s-itsol.net>
2017-02-05 03:53:25 +0900

    springライブラリ追加

---
+++ src/main/java/net/sitsol/victoria/setvlet/VctDispatcherServlet.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package net.sitsol.victoria.setvlet;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ *
+ * @author shibano
+ */
+public class VctDispatcherServlet extends DispatcherServlet {
+
+	private static final long serialVersionUID = 1L;
+
+}
