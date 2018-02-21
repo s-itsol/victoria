7a9c964524e0a1c96b5f92a977e4cb849d10a3a9
rei.shibano <shibano@s-itsol.net>
2018-02-21 23:39:02 +0900

    いろいろ

--- src/main/java/net/sitsol/victoria/setvlet/spring/VctSpringHandlerInterceptor.java
+++ src/main/java/net/sitsol/victoria/setvlet/spring/VctSpringHandlerInterceptor.java
@@ -3,15 +3,29 @@
  */
 package net.sitsol.victoria.setvlet.spring;
 
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sitsol.victoria.annotation.servlet.VctNoAuth;
+import net.sitsol.victoria.annotation.servlet.VctNoLogRequestUrl;
+import net.sitsol.victoria.configs.VctStaticApParam;
+import net.sitsol.victoria.consts.VctHttpConst;
+import net.sitsol.victoria.consts.VctLogKeywordConst;
+import net.sitsol.victoria.log4j.VctLogger;
+import net.sitsol.victoria.utils.statics.VctHttpUtils;
+import net.sitsol.victoria.utils.statics.VctServerUtils;
+import net.sitsol.victoria.utils.statics.VctSpringAnnotationUtils;
+import net.sitsol.victoria.utils.statics.VctSpringMvcUtils;
+
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 /**
  * Springハンドラ・インターセプタ
- *  ※Victoriaでは今のところ特に利用用途なし
  *
  * @author shibano
  */
@@ -29,28 +43,209 @@ public class VctSpringHandlerInterceptor extends HandlerInterceptorAdapter {
 	 */
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		// ※現状は特に処理なし
-		return true;
+		
+		// 静的コンテンツだった場合は何もしない
+		if ( handler instanceof ResourceHttpRequestHandler ) {
+			// ※特に処理なし
+			
+		// ハンドラメソッドだった場合 ※通常はこのルートしか来ない想定
+		} else if ( handler instanceof HandlerMethod ) {
+			
+			HandlerMethod handlerMethod = (HandlerMethod) handler;		// ハンドラメソッド
+			Method mappingMethod = handlerMethod.getMethod();			// マッピングメソッド
+			
+			// コントローラメソッド処理前イベントを実行
+			this.methodPreHandle(request, response, mappingMethod);
+			
+		// それ以外
+		} else {
+			// ※実装していない想定外なクラス型なので、警告ログを出力しておく
+			VctLogger.getLogger().warn("★コントローラ処理前イベント通知にて、想定外のハンドラ・インスタンス通知あり"
+											+ " - ハンドラクラス型：[" + ( handler != null ? handler.getClass().getSimpleName() : null ) + "]"
+										);
+		}
+		
+		return true;		// ※常に「処理を継続する」を返しておく
 	}
 
 	/**
 	 * コントローラ処理後イベント通知
+	 *  ※handlerがObject型なので、アプリ側でオーバーライド出来ないようfinalにしてある。
+	 *  
+	 * @param request HTTPサーブレットリクエスト
+	 * @param response HTTPサーブレットレスポンス
 	 * @param handler ハンドラのインスタンス
 	 * @param modelAndView モデル＆ビュー情報 ※コントローラ処理で当該インスタンスを返していない場合はnull
 	 */
 	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		// ※現状は特に処理なし
+	public final void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		
+		// 静的コンテンツだった場合は何もしない
+		if ( handler instanceof ResourceHttpRequestHandler ) {
+			// ※特に処理なし
+			
+		// ハンドラメソッドだった場合 ※通常はこのルートしか来ない想定
+		} else if ( handler instanceof HandlerMethod ) {
+			
+			HandlerMethod handlerMethod = (HandlerMethod) handler;		// ハンドラメソッド
+			Method mappingMethod = handlerMethod.getMethod();			// マッピングメソッド
+			
+			// コントローラメソッド処理後イベントを実行
+			this.methodPostHandle(request, response, mappingMethod, modelAndView);
+			
+		// それ以外
+		} else {
+			// ※実装していない想定外なクラス型なので、警告ログを出力しておく
+			VctLogger.getLogger().warn("★コントローラ処理後イベント通知にて、想定外のハンドラ・インスタンス通知あり"
+											+ " - ハンドラクラス型：[" + ( handler != null ? handler.getClass().getSimpleName() : null ) + "]"
+										);
+		}
 	}
 
 	/**
 	 * リクエスト完了イベント通知
+	 *  ※handlerがObject型なので、アプリ側でオーバーライド出来ないようfinalにしてある。
+	 *  
+	 * @param request HTTPサーブレットリクエスト
+	 * @param response HTTPサーブレットレスポンス
 	 * @param handler ハンドラのインスタンス
-	 * @param ex スローされた例外 ※スローされていない場合はnull
+	 * @param exception スローされた例外 ※スローされていない場合はnull
 	 */
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-		// ※現状は特に処理なし
+	public final void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
+		
+		// 静的コンテンツだった場合は何もしない
+		if ( handler instanceof ResourceHttpRequestHandler ) {
+			// ※特に処理なし
+			
+		// ハンドラメソッドだった場合 ※通常はこのルートしか来ない想定
+		} else if ( handler instanceof HandlerMethod ) {
+			
+			HandlerMethod handlerMethod = (HandlerMethod) handler;		// ハンドラメソッド
+			Method mappingMethod = handlerMethod.getMethod();			// マッピングメソッド
+			
+			// メソッドリクエスト完了イベント通知
+			this.methodAfterCompletion(request, response, mappingMethod, exception);
+			
+		// それ以外
+		} else {
+			// ※実装していない想定外なクラス型なので、警告ログを出力しておく
+			VctLogger.getLogger().warn("★コントローラ処理後イベント通知にて、想定外のハンドラ・インスタンス通知あり"
+											+ " - ハンドラクラス型：[" + ( handler != null ? handler.getClass().getSimpleName() : null ) + "]"
+										);
+		}
+	}
+
+	/**
+	 * コントローラメソッド処理前イベント通知
+	 * @param request HTTPサーブレットリクエスト
+	 * @param response HTTPサーブレットレスポンス
+	 * @param mappingMethod マッピングメソッド
+	 */
+	protected void methodPreHandle(HttpServletRequest request, HttpServletResponse response, Method mappingMethod) throws Exception {
+		
+		// システム共通リクエスト属性値設定
+		this.setCommonRequestAttributes(request, mappingMethod);
+		
+		// HTTPリクエストURLログ出力
+		this.httpRequestUrlLog(request, mappingMethod);
+		
+		// 認証
+		this.auth(request, mappingMethod);
+	}
+
+	/**
+	 * システム共通リクエスト属性値設定
+	 * @param request HTTPサーブレットリクエスト
+	 * @param mappingMethod マッピングメソッド
+	 */
+	protected void setCommonRequestAttributes(HttpServletRequest request, Method mappingMethod) throws Exception {
+		
+		// システム共通リクエスト属性値設定
+		request.setAttribute(VctHttpConst.ENV_NAME, VctStaticApParam.getInstance().getDispEnvName());		// 環境名
+		request.setAttribute(VctHttpConst.HOST_NAME, VctServerUtils.HOST_NAME);								// ホスト名
+	}
+
+	/**
+	 * HTTPリクエストURLログ出力
+	 * @param request HTTPサーブレットリクエスト
+	 * @param mappingMethod マッピングメソッド
+	 */
+	protected void httpRequestUrlLog(HttpServletRequest request, Method mappingMethod) throws Exception {
+		
+		if ( request == null ) { return; }
+		
+		// HTTPリクエストURLログ出力フラグOFF
+		if ( !VctStaticApParam.getInstance().isHttpRequestUrlLogOutputFlg() ) { return; }
+		
+		// HTTPリクエストURLログ出力不要アノテーションあり
+		if ( VctSpringMvcUtils.hasCurrentThreadAnnotation(VctNoLogRequestUrl.class) ) { return; }
+		
+		// HTTPリクエストURLログ出力
+		StringBuilder message = new StringBuilder();
+		message.append(VctLogKeywordConst.REQURL).append(" ").append(VctHttpUtils.createUrlQueryString(request));
+		
+		VctLogger.getLogger().info(message.toString());
+	}
+
+	/**
+	 * 認証
+	 *  ※デフォルトではログインユーザ情報を取得できない(≒ログイン状態ではない)場合にのみ「認証NG」として、
+	 *    セッションタイムアウト例外をスローする。
+	 *    各アプリ独自の認証判定は、本メソッドをオーバーライド実装することを想定しており、
+	 *    その判定NG時、さらに任意のページへ遷移させたい場合などは、
+	 *    独自の業務例外をスローしつつ、「例外ハンドラ実行」メソッドもオーバーライド実装することで可能なはず。
+	 * 
+	 * @param request HTTPサーブレットリクエスト
+	 * @param mappingMethod マッピングメソッド
+	 */
+	protected void auth(HttpServletRequest request, Method mappingMethod) throws Exception {
+		
+		if ( request == null ) { return; }
+		
+		// 認証不要アノテーションありは処理しない
+		if ( VctSpringAnnotationUtils.hasAnnotation(mappingMethod, VctNoAuth.class) ) {
+			return;
+		}
+		
+		// ●●●TODO：
+	}
+
+	/**
+	 * コントローラメソッド処理後イベント通知
+	 * @param request HTTPサーブレットリクエスト
+	 * @param response HTTPサーブレットレスポンス
+	 * @param mappingMethod マッピングメソッド
+	 * @param modelAndView モデル＆ビュー情報 ※コントローラ処理で当該インスタンスを返していない場合はnull
+	 */
+	protected void methodPostHandle(HttpServletRequest request, HttpServletResponse response, Method mappingMethod, ModelAndView modelAndView) throws Exception {
+		
+//		if ( modelAndView != null ) {
+//			
+//			String formName = AgoraThreadMappingMethodUtils.getFromMappingName();
+//			
+//			if ( formName != null ) {
+//				Object obj = modelAndView.getModelMap().get(formName);
+//				
+//				if ( obj != null ) {
+//					request.setAttribute(formName, obj);
+//				}
+//			}
+//		}
+//		セッションの属性名を、マッピングフォーム名(＝先頭が大文字)にしようとしたが無理だった
+		
+		
+	}
+
+	/**
+	 * メソッドリクエスト完了イベント通知
+	 * @param request HTTPサーブレットリクエスト
+	 * @param response HTTPサーブレットレスポンス
+	 * @param mappingMethod マッピングメソッド
+	 * @param exception スローされた例外 ※スローされていない場合はnull
+	 */
+	protected void methodAfterCompletion(HttpServletRequest request, HttpServletResponse response, Method mappingMethod, Exception exception) throws Exception {
+		// ※派生クラス側でオーバーライド実装させる想定なので、こちらでは特に処理なし。
 	}
 
 }
---
+++ src/main/java/net/sitsol/victoria/utils/statics/VctSpringAnnotationUtils.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package net.sitsol.victoria.utils.statics;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+import org.springframework.core.annotation.AnnotationUtils;
+
+/**
+ * Springのアノテーション支援ユーティリティ・ラッパー
+ * 
+ * @author shibano
+ */
+public class VctSpringAnnotationUtils {
+
+	/**
+	 * コンストラクタ ※外部からインスタンス化させない
+	 */
+	protected VctSpringAnnotationUtils() {}
+
+	/**
+	 * アノテーションあり判定
+	 * @param targetMethod 対象メソッド
+	 * @param targetAnnotationType 対象アノテーション
+	 * @return 判定結果 ※true：アノテーションあり
+	 */
+	public static <AnnotationClass extends Annotation> boolean hasAnnotation(Method targetMethod, Class<AnnotationClass> targetAnnotationType) {
+		// ※得られたら「アノテーションあり」
+		return ( findAnnotation(targetMethod, targetAnnotationType) != null );
+	}
+
+	/**
+	 * アノテーション取得
+	 * @param targetMethod 対象メソッド
+	 * @param targetAnnotationType 対象アノテーション
+	 * @return アノテーション ※得られなかった場合(≒対象アノテーションが未設定)はnull
+	 */
+	public static <AnnotationClass extends Annotation> AnnotationClass findAnnotation(Method targetMethod, Class<AnnotationClass> targetAnnotationType) {
+		
+		if ( targetMethod == null || targetAnnotationType == null ) {
+			return null;
+		}
+		
+		// 対象アノテーション取得 ※spring提供ユーティリティ
+		return AnnotationUtils.findAnnotation(targetMethod, targetAnnotationType);
+	}
+
+}
